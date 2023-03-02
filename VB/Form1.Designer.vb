f6e221fbd8b7bb87227ab4052afaeafca88a9802
DevExpressExampleBot <fakeEmail>
2023-03-02 17:41:03 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,144 +1,149 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace RichEditDataBoundScenario
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.richEditControl1 = New DevExpress.XtraRichEdit.RichEditControl()
-			Me.dataNavigator1 = New DevExpress.XtraEditors.DataNavigator()
-			Me.carsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-			Me.dataSet11 = New RichEditDataBoundScenario.DataSet1()
-			Me.carsTableAdapter = New RichEditDataBoundScenario.DataSet1TableAdapters.CarsTableAdapter()
-			Me.lblCurrentRecordInfo = New System.Windows.Forms.Label()
-			Me.btnRejectChanges = New DevExpress.XtraEditors.SimpleButton()
-			Me.btnAcceptChanges = New DevExpress.XtraEditors.SimpleButton()
-			Me.toolTip1 = New System.Windows.Forms.ToolTip(Me.components)
-			CType(Me.carsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.dataSet11, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' richEditControl1
-			' 
-			Me.richEditControl1.Anchor = (CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
-			Me.richEditControl1.Location = New System.Drawing.Point(9, 37)
-			Me.richEditControl1.Margin = New System.Windows.Forms.Padding(2)
-			Me.richEditControl1.Name = "richEditControl1"
-			Me.richEditControl1.Size = New System.Drawing.Size(892, 404)
-			Me.richEditControl1.TabIndex = 0
-'			Me.richEditControl1.ModifiedChanged += New System.EventHandler(Me.richEditControl1_ModifiedChanged);
-			' 
-			' dataNavigator1
-			' 
-			Me.dataNavigator1.Anchor = (CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles))
-			Me.dataNavigator1.Buttons.Append.Visible = False
-			Me.dataNavigator1.Buttons.NextPage.Visible = False
-			Me.dataNavigator1.Buttons.PrevPage.Visible = False
-			Me.dataNavigator1.Buttons.Remove.Visible = False
-			Me.dataNavigator1.DataSource = Me.carsBindingSource
-			Me.dataNavigator1.Location = New System.Drawing.Point(12, 455)
-			Me.dataNavigator1.Name = "dataNavigator1"
-			Me.dataNavigator1.Size = New System.Drawing.Size(889, 24)
-			Me.dataNavigator1.TabIndex = 1
-			Me.dataNavigator1.Text = "dataNavigator1"
-'			Me.dataNavigator1.ButtonClick += New DevExpress.XtraEditors.NavigatorButtonClickEventHandler(Me.dataNavigator1_ButtonClick);
-			' 
-			' carsBindingSource
-			' 
-			Me.carsBindingSource.DataMember = "Cars"
-			Me.carsBindingSource.DataSource = Me.dataSet11
-			' 
-			' dataSet11
-			' 
-			Me.dataSet11.DataSetName = "DataSet1"
-			Me.dataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-			' 
-			' carsTableAdapter
-			' 
-			Me.carsTableAdapter.ClearBeforeFill = True
-			' 
-			' lblCurrentRecordInfo
-			' 
-			Me.lblCurrentRecordInfo.AutoSize = True
-			Me.lblCurrentRecordInfo.Location = New System.Drawing.Point(13, 13)
-			Me.lblCurrentRecordInfo.Name = "lblCurrentRecordInfo"
-			Me.lblCurrentRecordInfo.Size = New System.Drawing.Size(94, 13)
-			Me.lblCurrentRecordInfo.TabIndex = 2
-			Me.lblCurrentRecordInfo.Text = "Current record info"
-			' 
-			' btnRejectChanges
-			' 
-			Me.btnRejectChanges.Location = New System.Drawing.Point(298, 8)
-			Me.btnRejectChanges.Name = "btnRejectChanges"
-			Me.btnRejectChanges.Size = New System.Drawing.Size(106, 23)
-			Me.btnRejectChanges.TabIndex = 3
-			Me.btnRejectChanges.Text = "Reject Changes"
-			Me.toolTip1.SetToolTip(Me.btnRejectChanges, "Apply original content from the database to the current record")
-'			Me.btnRejectChanges.Click += New System.EventHandler(Me.btnRejectChanges_Click);
-			' 
-			' btnAcceptChanges
-			' 
-			Me.btnAcceptChanges.Location = New System.Drawing.Point(186, 8)
-			Me.btnAcceptChanges.Name = "btnAcceptChanges"
-			Me.btnAcceptChanges.Size = New System.Drawing.Size(106, 23)
-			Me.btnAcceptChanges.TabIndex = 4
-			Me.btnAcceptChanges.Text = "Accept Changes"
-			Me.toolTip1.SetToolTip(Me.btnAcceptChanges, "Save all records to the database")
-'			Me.btnAcceptChanges.Click += New System.EventHandler(Me.btnAcceptChanges_Click);
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(910, 499)
-			Me.Controls.Add(Me.btnAcceptChanges)
-			Me.Controls.Add(Me.btnRejectChanges)
-			Me.Controls.Add(Me.lblCurrentRecordInfo)
-			Me.Controls.Add(Me.dataNavigator1)
-			Me.Controls.Add(Me.richEditControl1)
-			Me.Margin = New System.Windows.Forms.Padding(2)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.carsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.dataSet11, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
-			Me.PerformLayout()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+'#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.richEditControl1 = New DevExpress.XtraRichEdit.RichEditControl()
+            Me.dataNavigator1 = New DevExpress.XtraEditors.DataNavigator()
+            Me.carsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+            Me.dataSet11 = New RichEditDataBoundScenario.DataSet1()
+            Me.carsTableAdapter = New RichEditDataBoundScenario.DataSet1TableAdapters.CarsTableAdapter()
+            Me.lblCurrentRecordInfo = New System.Windows.Forms.Label()
+            Me.btnRejectChanges = New DevExpress.XtraEditors.SimpleButton()
+            Me.btnAcceptChanges = New DevExpress.XtraEditors.SimpleButton()
+            Me.toolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+            CType((Me.carsBindingSource), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.dataSet11), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' richEditControl1
+            ' 
+            Me.richEditControl1.Anchor = CType(((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+            Me.richEditControl1.Location = New System.Drawing.Point(9, 37)
+            Me.richEditControl1.Margin = New System.Windows.Forms.Padding(2)
+            Me.richEditControl1.Name = "richEditControl1"
+            Me.richEditControl1.Size = New System.Drawing.Size(892, 404)
+            Me.richEditControl1.TabIndex = 0
+            AddHandler Me.richEditControl1.ModifiedChanged, New System.EventHandler(AddressOf Me.richEditControl1_ModifiedChanged)
+            ' 
+            ' dataNavigator1
+            ' 
+            Me.dataNavigator1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
+            Me.dataNavigator1.Buttons.Append.Visible = False
+            Me.dataNavigator1.Buttons.NextPage.Visible = False
+            Me.dataNavigator1.Buttons.PrevPage.Visible = False
+            Me.dataNavigator1.Buttons.Remove.Visible = False
+            Me.dataNavigator1.DataSource = Me.carsBindingSource
+            Me.dataNavigator1.Location = New System.Drawing.Point(12, 455)
+            Me.dataNavigator1.Name = "dataNavigator1"
+            Me.dataNavigator1.Size = New System.Drawing.Size(889, 24)
+            Me.dataNavigator1.TabIndex = 1
+            Me.dataNavigator1.Text = "dataNavigator1"
+            AddHandler Me.dataNavigator1.ButtonClick, New DevExpress.XtraEditors.NavigatorButtonClickEventHandler(AddressOf Me.dataNavigator1_ButtonClick)
+            ' 
+            ' carsBindingSource
+            ' 
+            Me.carsBindingSource.DataMember = "Cars"
+            Me.carsBindingSource.DataSource = Me.dataSet11
+            ' 
+            ' dataSet11
+            ' 
+            Me.dataSet11.DataSetName = "DataSet1"
+            Me.dataSet11.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+            ' 
+            ' carsTableAdapter
+            ' 
+            Me.carsTableAdapter.ClearBeforeFill = True
+            ' 
+            ' lblCurrentRecordInfo
+            ' 
+            Me.lblCurrentRecordInfo.AutoSize = True
+            Me.lblCurrentRecordInfo.Location = New System.Drawing.Point(13, 13)
+            Me.lblCurrentRecordInfo.Name = "lblCurrentRecordInfo"
+            Me.lblCurrentRecordInfo.Size = New System.Drawing.Size(94, 13)
+            Me.lblCurrentRecordInfo.TabIndex = 2
+            Me.lblCurrentRecordInfo.Text = "Current record info"
+            ' 
+            ' btnRejectChanges
+            ' 
+            Me.btnRejectChanges.Location = New System.Drawing.Point(298, 8)
+            Me.btnRejectChanges.Name = "btnRejectChanges"
+            Me.btnRejectChanges.Size = New System.Drawing.Size(106, 23)
+            Me.btnRejectChanges.TabIndex = 3
+            Me.btnRejectChanges.Text = "Reject Changes"
+            Me.toolTip1.SetToolTip(Me.btnRejectChanges, "Apply original content from the database to the current record")
+            AddHandler Me.btnRejectChanges.Click, New System.EventHandler(AddressOf Me.btnRejectChanges_Click)
+            ' 
+            ' btnAcceptChanges
+            ' 
+            Me.btnAcceptChanges.Location = New System.Drawing.Point(186, 8)
+            Me.btnAcceptChanges.Name = "btnAcceptChanges"
+            Me.btnAcceptChanges.Size = New System.Drawing.Size(106, 23)
+            Me.btnAcceptChanges.TabIndex = 4
+            Me.btnAcceptChanges.Text = "Accept Changes"
+            Me.toolTip1.SetToolTip(Me.btnAcceptChanges, "Save all records to the database")
+            AddHandler Me.btnAcceptChanges.Click, New System.EventHandler(AddressOf Me.btnAcceptChanges_Click)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(910, 499)
+            Me.Controls.Add(Me.btnAcceptChanges)
+            Me.Controls.Add(Me.btnRejectChanges)
+            Me.Controls.Add(Me.lblCurrentRecordInfo)
+            Me.Controls.Add(Me.dataNavigator1)
+            Me.Controls.Add(Me.richEditControl1)
+            Me.Margin = New System.Windows.Forms.Padding(2)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.carsBindingSource), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.dataSet11), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+            Me.PerformLayout()
+        End Sub
 
-		Private WithEvents richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
-		Private dataSet11 As DataSet1
-		Private WithEvents dataNavigator1 As DevExpress.XtraEditors.DataNavigator
-		Private carsBindingSource As System.Windows.Forms.BindingSource
-		Private carsTableAdapter As RichEditDataBoundScenario.DataSet1TableAdapters.CarsTableAdapter
-		Private lblCurrentRecordInfo As System.Windows.Forms.Label
-		Private WithEvents btnRejectChanges As DevExpress.XtraEditors.SimpleButton
-		Private toolTip1 As System.Windows.Forms.ToolTip
-		Private WithEvents btnAcceptChanges As DevExpress.XtraEditors.SimpleButton
-	End Class
+'#End Region
+        Private richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
+
+        Private dataSet11 As RichEditDataBoundScenario.DataSet1
+
+        Private dataNavigator1 As DevExpress.XtraEditors.DataNavigator
+
+        Private carsBindingSource As System.Windows.Forms.BindingSource
+
+        Private carsTableAdapter As RichEditDataBoundScenario.DataSet1TableAdapters.CarsTableAdapter
+
+        Private lblCurrentRecordInfo As System.Windows.Forms.Label
+
+        Private btnRejectChanges As DevExpress.XtraEditors.SimpleButton
+
+        Private toolTip1 As System.Windows.Forms.ToolTip
+
+        Private btnAcceptChanges As DevExpress.XtraEditors.SimpleButton
+    End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,71 +1,61 @@
-Imports Microsoft.VisualBasic
 Imports System
 Imports System.Data
 Imports System.Windows.Forms
 Imports DevExpress.XtraEditors
-Imports DevExpress.XtraRichEdit
 
 Namespace RichEditDataBoundScenario
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			' TODO: This line of code loads data into the 'dataSet11.Cars' table. You can move, or remove it, as needed.
-			Me.carsTableAdapter.Fill(Me.dataSet11.Cars)
+    Public Partial Class Form1
+        Inherits Form
 
-			' Setup binding only when datasource is filled with data to minimize number of UpdateCommandsState() method calls
-			richEditControl1.DataBindings.Add(New Binding("RtfText", Me.carsBindingSource, "RtfContent"))
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			AddHandler dataSet11.Cars.ColumnChanged, AddressOf Cars_ColumnChanged
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ' TODO: This line of code loads data into the 'dataSet11.Cars' table. You can move, or remove it, as needed.
+            carsTableAdapter.Fill(dataSet11.Cars)
+            ' Setup binding only when datasource is filled with data to minimize number of UpdateCommandsState() method calls
+            richEditControl1.DataBindings.Add(New Binding("RtfText", carsBindingSource, "RtfContent"))
+            AddHandler dataSet11.Cars.ColumnChanged, New DataColumnChangeEventHandler(AddressOf Cars_ColumnChanged)
+            UpdateCommandsState()
+        End Sub
 
-			UpdateCommandsState()
-		End Sub
+        Private Sub Cars_ColumnChanged(ByVal sender As Object, ByVal e As DataColumnChangeEventArgs)
+            If Not richEditControl1.Modified Then e.Row.AcceptChanges()
+        End Sub
 
-		Private Sub Cars_ColumnChanged(ByVal sender As Object, ByVal e As DataColumnChangeEventArgs)
-			If (Not richEditControl1.Modified) Then
-				e.Row.AcceptChanges()
-			End If
-		End Sub
+        Private Sub dataNavigator1_ButtonClick(ByVal sender As Object, ByVal e As NavigatorButtonClickEventArgs)
+            If e.Button.ButtonType = NavigatorButtonType.EndEdit Then richEditControl1.Modified = False
+            BeginInvoke(New MethodInvoker(AddressOf UpdateCommandsState))
+        End Sub
 
-		Private Sub dataNavigator1_ButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.NavigatorButtonClickEventArgs) Handles dataNavigator1.ButtonClick
-			If e.Button.ButtonType = NavigatorButtonType.EndEdit Then
-				richEditControl1.Modified = False
-			End If
+        Private Sub richEditControl1_ModifiedChanged(ByVal sender As Object, ByVal e As EventArgs)
+            UpdateCommandsState()
+        End Sub
 
-			Me.BeginInvoke(New MethodInvoker(AddressOf UpdateCommandsState))
-		End Sub
+        Private Sub btnAcceptChanges_Click(ByVal sender As Object, ByVal e As EventArgs)
+            carsTableAdapter.Update(dataSet11.Cars)
+            dataSet11.Cars.AcceptChanges()
+            UpdateCommandsState()
+        End Sub
 
-		Private Sub richEditControl1_ModifiedChanged(ByVal sender As Object, ByVal e As EventArgs) Handles richEditControl1.ModifiedChanged
-			UpdateCommandsState()
-		End Sub
+        Private Sub btnRejectChanges_Click(ByVal sender As Object, ByVal e As EventArgs)
+            Dim currentDataRow As DataRow = dataSet11.Cars.Rows(carsBindingSource.Position)
+            currentDataRow.RejectChanges()
+            UpdateCommandsState()
+        End Sub
 
-		Private Sub btnAcceptChanges_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnAcceptChanges.Click
-			Me.carsTableAdapter.Update(Me.dataSet11.Cars)
-			Me.dataSet11.Cars.AcceptChanges()
-			UpdateCommandsState()
-		End Sub
-
-		Private Sub btnRejectChanges_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnRejectChanges.Click
-			Dim currentDataRow As DataRow = Me.dataSet11.Cars.Rows(carsBindingSource.Position)
-			currentDataRow.RejectChanges()
-			UpdateCommandsState()
-		End Sub
-
-		Private Sub UpdateCommandsState()
-			' Update control level commands state
-			dataNavigator1.Buttons.CancelEdit.Enabled = richEditControl1.Modified
-			dataNavigator1.Buttons.EndEdit.Enabled = richEditControl1.Modified
-
-			' Update data (in-memory) level commands state
-			Dim currentDataRow As DataRow = Me.dataSet11.Cars.Rows(carsBindingSource.Position)
-			btnRejectChanges.Enabled = (currentDataRow.RowState <> DataRowState.Unchanged) AndAlso Not richEditControl1.Modified
-			btnAcceptChanges.Enabled = Me.dataSet11.HasChanges() AndAlso Not richEditControl1.Modified
-
-			' Update info panel
-			lblCurrentRecordInfo.Text = String.Format("Current Record: {0}{1}", carsBindingSource.Position, (IIf(btnRejectChanges.Enabled, "*", "")))
-		End Sub
-	End Class
+        Private Sub UpdateCommandsState()
+            ' Update control level commands state
+            dataNavigator1.Buttons.CancelEdit.Enabled = richEditControl1.Modified
+            dataNavigator1.Buttons.EndEdit.Enabled = richEditControl1.Modified
+            ' Update data (in-memory) level commands state
+            Dim currentDataRow As DataRow = dataSet11.Cars.Rows(carsBindingSource.Position)
+            btnRejectChanges.Enabled = currentDataRow.RowState <> DataRowState.Unchanged AndAlso Not richEditControl1.Modified
+            btnAcceptChanges.Enabled = dataSet11.HasChanges() AndAlso Not richEditControl1.Modified
+            ' Update info panel
+            lblCurrentRecordInfo.Text = String.Format("Current Record: {0}{1}", carsBindingSource.Position, If(btnRejectChanges.Enabled, "*", ""))
+        End Sub
+    End Class
 End Namespace
